52a880c8a8b11e7c0c8cffa7af38c66cd1d5832b
rmpestano <rmpestano@gmail.com>
2017-09-30 07:51:41 -0300

    refs #101

---
+++ maven-install.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+mvn clean package deploy --settings settings.xml
+mvn clean package deploy -Pdev --settings settings.xml
+mvn clean package deploy -Pno-cache --settings settings.xml
+mvn clean package deploy -Pno-cache-dev --settings settings.xml
+mvn clean package deploy -Pwithout-bootstrap --settings settings.xml
+mvn clean package deploy -Pwithout-bootstrap-dev --settings settings.xml
+mvn clean package deploy -Pwithout-jsf --settings settings.xml
+mvn clean package deploy -Pwithout-jsf-dev --settings settings.xml
