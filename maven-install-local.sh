5bea7b28c15f5b3bd22b961654516d01b3ac0a6c
rmpestano <rmpestano@gmail.com>
2018-06-04 19:36:33 -0300

    perf

--- maven-install-local.sh
+++ maven-install-local.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 mvn clean install --settings settings.xml
+mvn clean install -Pdev --settings settings.xml
+mvn clean install -Pno-cache --settings settings.xml
 mvn clean install -Pno-cache-dev --settings settings.xml
 mvn clean install -Pwithout-bootstrap --settings settings.xml
 mvn clean install -Pwithout-bootstrap-dev --settings settings.xml
 mvn clean install -Pwithout-jsf --settings settings.xml
 mvn clean install -Pwithout-jsf-dev --settings settings.xml
-mvn clean install -Pdev --settings settings.xml
-mvn clean install -Pno-cache --settings settings.xml
