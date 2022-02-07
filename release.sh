a7cc28736d778dbffba85de29acbab347054a2b3
rmpestano <rpestano@mobiquityinc.com>
2022-02-07 09:40:37 +0100

    manualy prepare release 1.3.2

--- release.sh
+++ release.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-mvn clean install -Prelease && mvn install -Pdev -Prelease && mvn install -Pno-cache -Prelease mvn install -Pno-cache-dev -Prelease \
+mvn clean install -Prelease && mvn install -Pdev -Prelease && mvn install -Pno-cache -Prelease && mvn install -Pno-cache-dev -Prelease \
  && mvn install -Pwithout-jsf -Prelease && mvn install -Pwithout-jsf-dev -Prelease && mvn install -Pno-fonts -Prelease && mvn install -Pno-fonts-dev -Prelease \
  && mvn install -Pno-cache-no-fonts -Prelease && mvn deploy -Prelease
