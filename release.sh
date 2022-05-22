18d41605e730b54d675a4cf2dcca26d289e30567
rmpestano <rpestano@mobiquityinc.com>
2022-05-22 09:53:01 +0200

    refs #256

--- release.sh
+++ release.sh
@@ -2,4 +2,4 @@
 
 mvn clean install -Prelease && mvn install -Pdev -Prelease && mvn install -Pno-cache -Prelease && mvn install -Pno-cache-dev -Prelease \
  && mvn install -Pwithout-jsf -Prelease && mvn install -Pwithout-jsf-dev -Prelease && mvn install -Pno-fonts -Prelease && mvn install -Pno-fonts-dev -Prelease \
- && mvn install -Pno-cache-no-fonts -Prelease && mvn deploy -Prelease
+ && mvn install -Pno-cache-no-fonts -Prelease && mvn install -Pwithout-bootstrap -Prelease && mvn install -Pwithout-bootstrap-dev -Prelease && mvn deploy -Prelease
