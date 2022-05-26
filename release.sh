98c9bfcae1919e3787163556d64953c5751a40f1
rmpestano <rpestano@mobiquityinc.com>
2022-05-26 14:15:38 +0200

    Fix release issue

--- release.sh
+++ release.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-mvn clean install -Prelease && mvn clean install -Pdev -Prelease && mvn clean install -Pno-cache -Prelease && mvn clean install -Pno-cache-dev -Prelease \
- && mvn clean install -Pwithout-jsf -Prelease && mvn clean install -Pwithout-jsf-dev -Prelease && mvn clean install -Pno-fonts -Prelease && mvn clean install -Pno-fonts-dev -Prelease \
- && mvn clean install -Pno-cache-no-fonts -Prelease && mvn clean install -Pwithout-bootstrap -Prelease && mvn clean install -Pwithout-bootstrap-dev -Prelease && mvn clean deploy -Prelease
+mvn clean install -Prelease && mvn  install -Pdev -Prelease && mvn  install -Pno-cache -Prelease && mvn  install -Pno-cache-dev -Prelease \
+ && mvn  install -Pwithout-jsf -Prelease && mvn  install -Pwithout-jsf-dev -Prelease && mvn  install -Pno-fonts -Prelease && mvn  install -Pno-fonts-dev -Prelease \
+ && mvn  install -Pno-cache-no-fonts -Prelease && mvn  install -Pwithout-bootstrap -Prelease && mvn  install -Pwithout-bootstrap-dev -Prelease && mvn  deploy -Prelease
